31018e0e142f7615b6fc606eca74317999422585
qzziKryt <88944964+qzziKryt@users.noreply.github.com>
2024-01-09 18:39:33 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 module.exports = (client) => {
     console.log(`${client.user.tag} is online!`);
-    client.user.setActivity(`Standoff 2`, {type: "PLAYING"});
+    client.user.setActivity(`за вами котиками 😻`, {type: "WATCHING"});
 };
