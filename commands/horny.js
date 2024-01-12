fbad6ae2a1649b8904dc208308fb45d9be69faba
qzziKryt <88944964+qzziKryt@users.noreply.github.com>
2024-01-12 22:43:29 +0200

    Update horny.js

--- commands/horny.js
+++ commands/horny.js
@@ -2,29 +2,37 @@ const { MessageEmbed, MessageAttachment } = require("discord.js");
 const fetch = require('node-fetch');
 const fs = require("fs");
 
-exports.execute = (client, message, args) => {
-  if (!message.mentions.users.size) { // если написано $wasted, то показываем аватарку автора сообщения
- 	let link = `https://some-random-api.ml/canvas/gay/?avatar=${message.author.avatarURL({ format: 'png'})}` // Считываем эффект и говорим что картинка для эффекта - аватарка
-	const attachmentt = new MessageAttachment(link, 'horny.gif');
- 	const embed = new MessageEmbed()
-	 .setTitle(`${message.author.username} horny!`)
-	 .attachFiles(attachmentt)
-    	 .setColor('#rrggbb')
-	 .setImage('attachment://horny.gif')
-	return message.channel.send(embed);
-	}
-	const WastedList = message.mentions.users.map(user => { // Если написали $wasted @test, то показываем триггер аватарки пользователя test
-	let link = `https://some-random-api.ml/canvas/gay/?avatar=${user.avatarURL({ format: 'png'})}`
-  	const attachmentt = new MessageAttachment(link, 'horny.gif');
-	const embed = new MessageEmbed()
-  	 .setTitle(`${user.username} horny!`)
-  	 .attachFiles(attachmentt)
-         .setColor("#rrggbb")
-  	 .setImage('attachment://horny.gif')
-  	return embed
-	});
-	message.channel.send(WastedList);
-	return
+exports.execute = async (client, message, args) => {
+  try {
+    if (!message.mentions.users.size) {
+      let link = `https://some-random-api.ml/canvas/gay/?avatar=${message.author.avatarURL({ format: 'png'})}`;
+      console.log(link); // Log the link to the console
+      const attachmentt = new MessageAttachment(link, 'horny.gif');
+      const embed = new MessageEmbed()
+        .setTitle(`${message.author.username} horny!`)
+        .attachFiles(attachmentt)
+        .setColor('#FF5733') // Replace with a valid hex color code
+        .setImage('attachment://horny.gif');
+      return message.channel.send(embed);
+    }
+
+    const WastedList = message.mentions.users.map(user => {
+      let link = `https://some-random-api.ml/canvas/gay/?avatar=${user.avatarURL({ format: 'png'})}`;
+      console.log(link); // Log the link to the console
+      const attachmentt = new MessageAttachment(link, 'horny.gif');
+      const embed = new MessageEmbed()
+        .setTitle(`${user.username} horny!`)
+        .attachFiles(attachmentt)
+        .setColor("#FF5733") // Replace with a valid hex color code
+        .setImage('attachment://horny.gif');
+      return embed;
+    });
+
+    message.channel.send(WastedList);
+  } catch (error) {
+    console.error("Error in 'horny' command:", error);
+    message.channel.send("An error occurred while processing the command.");
+  }
 }
 
 exports.help = {
