69190fa24f165a644f8a1f6d1a835595ca1c5fc1
qzziKryt <88944964+qzziKryt@users.noreply.github.com>
2024-01-12 22:14:06 +0200

    Create horny.js

---
+++ commands/horny.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require("discord.js");
+
+exports.execute = (client, message, args) => {
+  if (!message.mentions.users.size) { // если написано $wasted, то показываем аватарку автора сообщения
+		let link = `https://some-random-api.ml/canvas/horny?avatar=${message.author.avatarURL({ format: 'png'})}` // Считываем эффект и говорим что картинка для эффекта - аватарка
+		const attachmentt = new Discord.MessageAttachment(link, 'horny.gif');
+		const embed = new Discord.MessageEmbed()
+			.setTitle(`${message.author.username} horny!`)
+		  .attachFiles(attachmentt)
+      .setColor('#rrggbb')
+		  .setImage('attachment://horny.gif')
+		return message.channel.send(embed);
+	}
+	const WastedList = message.mentions.users.map(user => { // Если написали $wasted @test, то показываем триггер аватарки пользователя test
+	  let link = `https://some-random-api.ml/canvas/horny?avatar=${user.avatarURL({ format: 'png'})}`
+  	const attachmentt = new Discord.MessageAttachment(link, 'horny.gif');
+  	const embed = new Discord.MessageEmbed()
+  		.setTitle(`${user.username} horny!`)
+  		.attachFiles(attachmentt)
+      .setColor("#rrggbb")
+  		.setImage('attachment://horny.gif')
+  	return embed
+	});
+	message.channel.send(WastedList);
+	return
+}
+
+exports.help = {
+    name: "horny",
+    aliases: [""],
+    usage: `horny @user`
+}
