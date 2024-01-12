88eadadac43826e5bc4a7e17b92488a8a6786216
qzziKryt <88944964+qzziKryt@users.noreply.github.com>
2024-01-12 22:28:45 +0200

    Update horny.js

--- commands/horny.js
+++ commands/horny.js
@@ -4,23 +4,23 @@ const fs = require("fs");
 
 exports.execute = (client, message, args) => {
   if (!message.mentions.users.size) { // если написано $wasted, то показываем аватарку автора сообщения
-		let link = `https://some-random-api.ml/canvas/horny?avatar=${message.author.avatarURL({ format: 'png'})}` // Считываем эффект и говорим что картинка для эффекта - аватарка
-		const attachmentt = new Discord.MessageAttachment(link, 'horny.gif');
-		const embed = new Discord.MessageEmbed()
-			.setTitle(`${message.author.username} horny!`)
-		  .attachFiles(attachmentt)
-      .setColor('#rrggbb')
-		  .setImage('attachment://horny.gif')
-		return message.channel.send(embed);
+ 	let link = `https://some-random-api.ml/canvas/horny?avatar=${message.author.avatarURL({ format: 'png'})}` // Считываем эффект и говорим что картинка для эффекта - аватарка
+	const attachmentt = new MessageAttachment(link, 'horny.gif');
+ 	const embed = new MessageEmbed()
+	 .setTitle(`${message.author.username} horny!`)
+	 .attachFiles(attachmentt)
+    	 .setColor('#rrggbb')
+	 .setImage('attachment://horny.gif')
+	return message.channel.send(embed);
 	}
 	const WastedList = message.mentions.users.map(user => { // Если написали $wasted @test, то показываем триггер аватарки пользователя test
-	  let link = `https://some-random-api.ml/canvas/horny?avatar=${user.avatarURL({ format: 'png'})}`
-  	const attachmentt = new Discord.MessageAttachment(link, 'horny.gif');
-  	const embed = new Discord.MessageEmbed()
-  		.setTitle(`${user.username} horny!`)
-  		.attachFiles(attachmentt)
-      .setColor("#rrggbb")
-  		.setImage('attachment://horny.gif')
+	let link = `https://some-random-api.ml/canvas/horny?avatar=${user.avatarURL({ format: 'png'})}`
+  	const attachmentt = new MessageAttachment(link, 'horny.gif');
+	const embed = new MessageEmbed()
+  	 .setTitle(`${user.username} horny!`)
+  	 .attachFiles(attachmentt)
+         .setColor("#rrggbb")
+  	 .setImage('attachment://horny.gif')
   	return embed
 	});
 	message.channel.send(WastedList);
