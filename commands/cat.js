7d1d97761b3711985086ac488a7ea4cf83e0e4ba
qzziKryt <88944964+qzziKryt@users.noreply.github.com>
2024-01-19 22:37:12 +0200

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -3,17 +3,25 @@ const { MessageEmbed } = require("discord.js");
 const fetch = require('node-fetch');
 
 exports.execute = async (client, message, args) => {
-  let res = await fetch('https://some-random-api.ml/img/cat') // Добавлено await
-    .then(res => res.json())
-    .then(json => {
-      const embed = new Discord.MessageEmbed()
-        .setColor("#917898")
-        .setTitle(`${message.guild.name}, котики :)`)
-        .setImage(json.link);
-      message.channel.send(embed);
-    });
-}
+  try {
+    let res = await fetch('https://some-random-api.ml/img/cat');
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
 
+    let json = await res.json();
+    
+    const embed = new Discord.MessageEmbed()
+      .setColor("#917898")
+      .setTitle(`${message.guild.name}, котики :)`)
+      .setImage(json.link);
+
+    message.channel.send(embed);
+  } catch (error) {
+    console.error('An error occurred:', error);
+    message.channel.send('An error occurred while processing the command.');
+  }
+}
 
 exports.help = {
     name: "cat",
