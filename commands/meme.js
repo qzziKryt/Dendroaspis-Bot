131e4d90ac67ad29a012e466fcfaf4ed29bce973
qzziKryt <88944964+qzziKryt@users.noreply.github.com>
2024-01-20 00:03:20 +0200

    Create meme.js

---
+++ commands/meme.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const fetch = require('node-fetch');
+
+exports.execute = async (client, message, args) => {
+    try {
+        const Reds = [
+            "memes",
+            "me_irl",
+            "dankmemes",
+            "comedyheaven",
+            "Animemes"
+        ];
+
+        const Rads = Reds[Math.floor(Math.random() * Reds.length)];
+
+        const res = await Fetch(`https://www.reddit.com/r/${Rads}/random/.json`);
+
+        const json = await res.json();
+
+        if (!json[0]) return message.channel.send(`Your Life Lmfao`);
+
+        const data = json[0].data.children[0].data;
+
+        const Embed = new MessageEmbed()
+            .setColor("rrggbb")
+            .setURL(`https://reddit.com${data.permalink}`)
+            .setTitle(data.title)
+            .setDescription(`Author : ${data.author}`)
+            .setImage(data.url)
+            .setFooter(`${data.ups || 0} 👍 | ${data.downs || 0} 👎 | ${data.num_comments || 0} 💬`)
+            .setTimestamp();
+
+        return message.channel.send(Embed);
+    }
+};
+
+exports.help = {
+    name: "meme",
+    aliases: [],
+    usage: `meme`
+};
