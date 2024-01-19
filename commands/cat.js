a866e20fc3215b8fbd20e42db92b947c5081c1c3
qzziKryt <88944964+qzziKryt@users.noreply.github.com>
2024-01-19 22:31:06 +0200

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -1,18 +1,19 @@
 const Discord = module.require("discord.js");
 const { MessageEmbed } = require("discord.js");
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 
-exports.execute = (client, message, args) => {
-  let res = fetch('https://some-random-api.ml/img/cat') //Извлекаем json от сайта
-  .then(res => res.json()) // Просматриваем текст
-  .then(json => {
-    const embed = new Discord.MessageEmbed()
-     .setColor("#917898")
-     .setTitle(`${message.guild.name}, котики :)`)
-     .setImage(json.link) // Ставим лисичку картинкой!
-    message.channel.send(embed); // Отсылаем сообщение
-  }
+exports.execute = async (client, message, args) => {
+  let res = await fetch('https://some-random-api.ml/img/cat') // Добавлено await
+    .then(res => res.json())
+    .then(json => {
+      const embed = new Discord.MessageEmbed()
+        .setColor("#917898")
+        .setTitle(`${message.guild.name}, котики :)`)
+        .setImage(json.link);
+      message.channel.send(embed);
+    });
 }
+
 
 exports.help = {
     name: "cat",
